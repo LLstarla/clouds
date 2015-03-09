5cd4df7be799e5f5bd8bf6f9d8fc931bdb2dd4fd
LLstarla <lgentry1@engr-134-18.eastern.ewu.edu>
2015-03-09 13:25:45 -0700

    new clouds!
    yay!!

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+options = {
+  src: 'img/new-cloud.png', // path to image, the default is a base64 (you can see the actual string in sources)
+  maxWidth: 300, // max image's width
+  maxHeight: 200, // amx image's height
+  minClouds: 4, // minimum amount of clouds
+  maxClouds: 10, // maximum amount of clouds
+  skyHeight: null, // height of the container element
+  wind: true // animate clouds, default is false
+};
+
+$('#sky').jQlouds(options);
+
